1a7cf6f17f5640de5582235835b17a9b9412cbb0
ETLuis <lcampanogoldaro@gmail.com>
2022-09-26 10:21:16 +0200

    App Landscape vs Portrait 1.0

---
+++ app/src/main/java/com/dam2/landscapevsportrait/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dam2.landscapevsportrait
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
